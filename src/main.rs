42974962ad0c380310bdc346f9d8a2a417481246
margo <mimargosha@gmail.com>
2024-02-27 17:01:40 +0300

    Practice 3: done

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 mod camera;
 mod image;
-mod light;
 mod objects;
 mod parser;
+mod random;
 mod ray;
 mod trace;
 
 use parser::*;
+use rand::Rng;
 use trace::trace_ray;
 
 fn render(scene: &mut Scene) {
-    for i in 0..scene.image.width {
-        for j in 0..scene.image.height {
-            let u = (i as f32 + 0.5) / scene.image.width as f32 * 2.0 - 1.0;
-            let v = (j as f32 + 0.5) / scene.image.height as f32 * 2.0 - 1.0;
-            let ray = scene.camera.ray_to_point(u, v);
+    for step in 0..scene.n_samples {
+        for i in 0..scene.image.width {
+            for j in 0..scene.image.height {
+                let du = scene.generator.gen::<f32>();
+                let dv = scene.generator.gen::<f32>();
+                let u = (i as f32 + du) / scene.image.width as f32 * 2.0 - 1.0;
+                let v = (j as f32 + dv) / scene.image.height as f32 * 2.0 - 1.0;
+                let ray = scene.camera.ray_to_point(u, v);
 
-            if let Some(color) = trace_ray(scene, &ray, 0) {
-                scene.image.set(i, j, color);
+                let old_color = scene.image.get(i, j);
+                let color = trace_ray(scene, &ray, 0);
+                let step_f = step as f32;
+                let new_color = (old_color * step_f + color) / (step_f + 1.0);
+                scene.image.set(i, j, new_color);
             }
         }
     }
